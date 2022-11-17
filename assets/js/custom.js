4f173b1c3a9f57095c2b6dfe7318c019bbe88f7e
Hebert Richard Masseno Dias <hebertdev82@gmail.com>
2022-11-17 18:53:13 -0300

    contact form

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -302,50 +302,64 @@
     }
   });
 
+
+
+
+
+
+
+
     // Ajax Contact Form JS
-    var form = $("#contact-form");
-    var formMessages = $(".form-message");
+    // var form = $("#contact-form");
+    // var formMessages = $(".form-message");
 
-    $(form).submit(function (e) {
-      e.preventDefault();
-      var formData = form.serialize();
-      $.ajax({
-        type: "POST",
-        url: form.attr("action"),
-        data: formData,
-      })
-        .done(function (response) {
-          // Make sure that the formMessages div has the 'success' class.
-          $(formMessages).removeClass("alert alert-danger");
-          $(formMessages).addClass("alert alert-success fade show");
+    // $(form).submit(function (e) {
+    //   e.preventDefault();
+    //   var formData = form.serialize();
+    //   $.ajax({
+    //     type: "POST",
+    //     url: form.attr("action"),
+    //     data: formData,
+    //   })
+    //     .done(function (response) {
+    //       // Make sure that the formMessages div has the 'success' class.
+    //       $(formMessages).removeClass("alert alert-danger");
+    //       $(formMessages).addClass("alert alert-success fade show");
 
-          // Set the message text.
-          formMessages.html(
-            "<button type='button' class='btn-close' data-bs-dismiss='alert'>&times;</button>"
-          );
-          formMessages.append(response);
+    //       // Set the message text.
+    //       formMessages.html(
+    //         "<button type='button' class='btn-close' data-bs-dismiss='alert'>&times;</button>"
+    //       );
+    //       formMessages.append(response);
 
-          // Clear the form.
-          $("#contact-form input,#contact-form textarea").val("");
-        })
-        .fail(function (data) {
-          // Make sure that the formMessages div has the 'error' class.
-          $(formMessages).removeClass("alert alert-success");
-          $(formMessages).addClass("alert alert-danger fade show");
+    //       // Clear the form.
+    //       $("#contact-form input,#contact-form textarea").val("");
+    //     })
+    //     .fail(function (data) {
+    //       // Make sure that the formMessages div has the 'error' class.
+    //       $(formMessages).removeClass("alert alert-success");
+    //       $(formMessages).addClass("alert alert-danger fade show");
 
-          // Set the message text.
-          if (data.responseText !== "") {
-            formMessages.html(
-              "<button type='button' class='btn-close' data-bs-dismiss='alert'>&times;</button>"
-            );
-            formMessages.append(data.responseText);
-          } else {
-            $(formMessages).text(
-              "Oops! Aconteceu algo de errado e sua mensagem não foi enviada."
-            );
-          }
-        });
-    });
+    //       // Set the message text.
+    //       if (data.responseText !== "") {
+    //         formMessages.html(
+    //           "<button type='button' class='btn-close' data-bs-dismiss='alert'>&times;</button>"
+    //         );
+    //         formMessages.append(data.responseText);
+    //       } else {
+    //         $(formMessages).text(
+    //           "Oops! Aconteceu algo de errado e sua mensagem não foi enviada."
+    //         );
+    //       }
+    //     });
+    // });
+  
+  
+  
+  
+  
+  
+  
   
 
   // Datepicker
